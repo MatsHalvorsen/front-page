25abf458447f50e0f639c8ee6c76fdd0346bcefb
MatsHalvorsen <mats.halvorsen@outlook.com>
2022-02-05 15:41:12 +0100

    apple icon

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -1,2 +1,8 @@
 // This file is intentionally blank
 // Use this file to add JavaScript to your project
+
+
+//make a booking system 
+//fix the js for get in touch with us 
+//Log in, where you can see you car wash historic and what is done (this can be downloaded to pdf to show when you sell the car.
+//as a proof that taking care of the car, add monthly subscription etc.
